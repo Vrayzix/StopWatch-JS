4b2c81417cc7a2d9d2ec3ec41816f5a07b0619ad
Lanacer Youcef <lanacer.youcef.zxc22@gmail.com>
2024-01-01 23:08:12 +0100

    initialize repo

---
+++ script.js
@@ -0,0 +1,63 @@
+let startButton = document.getElementById("start-button");
+let stopButton = document.getElementById("stop-button");
+let resetButton = document.getElementById("reset-button");
+
+let audio = new Audio("start-sound.mp3");
+var Interval;
+startButton.onclick = function () {
+  clearInterval(Interval);
+  Interval = setInterval(startTimer, 10);
+  audio.play();
+};
+stopButton.onclick = function () {
+  StopTimer();
+  audio.play();
+};
+resetButton.onclick = function () {
+  ResetTimer();
+  audio.play();
+};
+
+let tensCount = 0;
+let secondsCount = 0;
+let minutesCount = 0;
+
+function startTimer() {
+  tensCount++;
+  if (tensCount <= 9) {
+    document.getElementById("tens").innerHTML = "0" + tensCount;
+  }
+  if (tensCount > 9) {
+    document.getElementById("tens").innerHTML = tensCount;
+  }
+  if (tensCount > 99) {
+    secondsCount++;
+    tensCount = 0;
+    document.getElementById("seconds").innerHTML = "0" + secondsCount;
+    document.getElementById("tens").innerHTML = "00";
+    // alert("stop");
+  }
+  if (secondsCount > 9) {
+    document.getElementById("seconds").innerHTML = secondsCount;
+  }
+  if (secondsCount >= 60) {
+    secondsCount = 0;
+    minutesCount++;
+    document.getElementById("seconds").innerHTML = "0" + secondsCount;
+    document.getElementById("minutes").innerHTML = "0" + minutesCount;
+  }
+  if(minutesCount > 9){
+    document.getElementById("minutes").innerHTML = minutesCount;
+  }
+}
+function StopTimer() {
+  clearInterval(Interval);
+}
+function ResetTimer() {
+  tensCount = 0;
+  secondsCount = 0;
+  document.getElementById("tens").innerHTML = "00";
+  document.getElementById("seconds").innerHTML = "00";
+  document.getElementById("minutes").innerHTML = "00";
+  clearInterval(Interval);
+}
